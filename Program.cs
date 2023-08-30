38b48f4bc7643edece1faa59312c5e1499637a6f
hunter <hunter@DESKTOP-MKSU7FO>
2023-08-30 18:26:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace problem14
+{
+    internal class Program
+    {
+        public static void InvertedLetterPatern(int number) 
+        {
+          for(int i = 65 + number - 1; i >= 65; i--) 
+            {
+            
+            for(int j = 1; j <= number; j++) 
+                {
+
+                    Console.Write((char)i);
+                
+                
+                }
+                number--;
+                Console.WriteLine();
+
+
+            }
+        
+        
+        }
+
+        static void Main(string[] args)
+        {
+            InvertedLetterPatern(5);
+          
+        }
+    }
+}
